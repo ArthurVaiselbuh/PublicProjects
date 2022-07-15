85ab6729850fda13f8d8f47d981aa5d1e628595b
Arthur <arthurv@mail.tau.ac.il>
2022-07-15 23:58:08 +0300

    Basic dotNetChat c# chat program

---
+++ dotNetChat/DotNetChat/DotNetChatConsole/Chat.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using log4net;
+using System.Net;
+
+namespace DotNetChatConsole
+{
+    internal class ChatMessage
+    {
+        private int messageSize;
+        private byte[]? message = null;
+
+        public byte[] MessageBytes {get=> message!; }
+
+        private ChatMessage() {}
+
+        public byte[] ToBytes()
+        {
+            using var memoryStream = new MemoryStream();
+            memoryStream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder(messageSize)));
+            memoryStream.Write(message);
+            return memoryStream.ToArray();
+           
+        }
+
+        public static ChatMessage FromBytes(byte[] bytes)
+        {
+            ChatMessage result = new ChatMessage();
+            result.messageSize = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(bytes));
+            var size = result.messageSize - sizeof(int);
+            result.message = new byte[size];
+            Array.Copy(bytes, sizeof(int), result.message, 0, size);
+            return result;
+        }
+        
+        public static ChatMessage GetMessageFromString(string message)
+        {
+            var encodedMessage = Encoding.UTF8.GetBytes(message);
+            ChatMessage chatMessage =new ChatMessage { message = encodedMessage, messageSize = encodedMessage.Length + sizeof(int) };
+            return chatMessage; 
+        }
+    }
+
+    internal class Chat
+    {
+        private static log4net.ILog log = log4net.LogManager.GetLogger(nameof(Chat));
+        private Socket socket;
+        public Chat(Socket socket)
+        {
+            this.socket = socket;
+        }
+
+        public async Task SendChatMessage(string message)
+        {
+            var encodedMessage = Encoding.UTF8.GetBytes(message);
+            log.Debug($"Sending message: <{message}>, encoded length: {encodedMessage.Length}, encoded is: {encodedMessage}");
+            var chatMessage = ChatMessage.GetMessageFromString(message);
+            var bytesSent = await socket.SendAsync(chatMessage.ToBytes(), SocketFlags.None);
+        }
+
+        public async Task<string> ReadChatMessage()
+        {
+            log.Debug("Waiting on message from socket");
+            var bytes = await ReadFullMessageFromSocket();
+            log.Debug("Read full message");
+            var chatMessage = ChatMessage.FromBytes(bytes);
+            var decodedMessage = Encoding.UTF8.GetString(chatMessage.MessageBytes);
+            log.Debug($"Received decoded message: {decodedMessage}");
+            return decodedMessage;
+        }
+
+        private async Task<byte[]> ReadFullMessageFromSocket()
+        {
+            var bytes = new byte[1024];
+            var recvSize = await socket.ReceiveAsync(bytes, SocketFlags.None);
+            var size = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(bytes));
+            if (size != recvSize)
+            {
+                throw new NotImplementedException("Not handling long messages for now");
+            }
+            return bytes;
+        }
+
+
+    }
+}
---
+++ dotNetChat/DotNetChat/DotNetChatConsole/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using DotNetChatConsole;
+
+namespace DotNetChat // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private static log4net.ILog log = log4net.LogManager.GetLogger("Main");
+
+        private static int CommunicationPort = 6651;
+
+        static void Main(string[] args)
+        {
+            AsyncMain(args).Wait();
+        }
+
+        static async Task AsyncMain(string[] args)
+        {
+            InitLogging();
+            Socket sock;
+            if (args[0] == "--server")
+            {
+                log.Info("Running as server");
+                sock = await GetSocket(true);
+            }
+            else
+            {
+                log.Info("Running as client");
+                sock = await GetSocket(false);
+            }
+            Chat chat = new Chat(sock);
+            var readTask = Task.Factory.StartNew(() => ReadLoop(chat));
+            var writeTask = Task.Factory.StartNew(() => WriteLoop(chat));
+            Task.WaitAll(readTask, writeTask);
+        }
+
+        private static async void ReadLoop(Chat chat)
+        {
+            while(true)
+            {
+                var message = await chat.ReadChatMessage();
+                log.Info($"Got message: {message}");
+            }
+        }
+        private static async void WriteLoop(Chat chat)
+        {
+            while (true)
+            {
+                log.Info("Waiting for user input");
+                var line = Console.ReadLine();
+                log.Debug($"Read line: {line}");
+                if (line == null)
+                {
+                    log.Info("Got null line, exiting");
+                    Environment.Exit(1);
+                }
+                await chat.SendChatMessage(line);
+            }
+        }
+
+        static async Task<Socket> GetSocket(bool isServer)
+        {
+            var endpoint = new IPEndPoint(IPAddress.Loopback, CommunicationPort);
+            var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            if (isServer)
+            {
+                socket.Bind(endpoint);
+                log.Info("Begin listening");
+                socket.Listen();
+                var accepted = await socket.AcceptAsync();
+                log.Info("Accepted connection");
+                socket.Dispose();
+                return accepted;
+            }
+            log.Info("Connecting to remote server");
+            await socket.ConnectAsync(endpoint);
+            log.Info("Connected");
+            return socket;
+        }
+
+        private static void InitLogging()
+        {
+            log4net.Config.BasicConfigurator.Configure();
+        }
+    }
+}
